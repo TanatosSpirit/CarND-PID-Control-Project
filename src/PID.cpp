e38bef1af797f884d9a3a3538a821521876a3b5c
Andrei Liutov <lytovandrei@gmail.com>
2022-03-10 23:25:04 +0300

    Add simple PID

--- src/PID.cpp
+++ src/PID.cpp
@@ -12,7 +12,11 @@ void PID::Init(double Kp_, double Ki_, double Kd_) {
   /**
    * TODO: Initialize PID coefficients (and errors, if needed)
    */
+  Kp = Kp_;
+  Kd = Kd_;
+  Ki = Ki_;
 
+  p_error = 0.0;
 }
 
 void PID::UpdateError(double cte) {
@@ -20,11 +24,14 @@ void PID::UpdateError(double cte) {
    * TODO: Update PID errors based on cte.
    */
 
+  d_error = cte - p_error;
+  i_error += cte;
+  p_error = cte;
 }
 
 double PID::TotalError() {
   /**
    * TODO: Calculate and return the total error
    */
-  return 0.0;  // TODO: Add your total error calc here!
+  return -Kp * p_error - Kd * d_error - Ki * i_error;  // TODO: Add your total error calc here!
 }
